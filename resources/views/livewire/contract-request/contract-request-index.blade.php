c32cd6ecabed8134149228006d66dee2478a5dec
Natalia <nataliakulyk123@gmail.com>
2026-07-29 13:30:09 +0300

    #522 fix

--- resources/views/livewire/contract-request/contract-request-index.blade.php
+++ resources/views/livewire/contract-request/contract-request-index.blade.php
@@ -120,7 +120,7 @@
                                 </td>
                                 <td class="index-table-td">
                                     <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300">
-                                        {{ contractTypeLabel($item->type) }}
+                                        {{ Type::resolveLabel($item->type) }}
                                     </span>
                                 </td>
                                 <td class="index-table-td text-sm text-gray-500 dark:text-gray-400">
